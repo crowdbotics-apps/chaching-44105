7e211211e4e4eaf6afe2a981777b2c9eeb2c99c4
Crowdbotics <team@crowdbotics.com>
2023-10-23 13:27:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chaching-44105",
+  url: "https://chaching-44105.botics.co",
+  api: "https://chaching-44105.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
